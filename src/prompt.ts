0150f3d20b634202aae94acef700f6006eaf94c3
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2023-01-23 19:27:52 +0300

    a

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,11 +1,10 @@
 import "./container.scss"
-import { Base, defineElement } from "@chocolatelibui/core"
-import { events, forDocuments } from "@chocolatelibui/document"
+import { Base } from "@chocolatelibui/core"
 
-export class Container extends Base {
+export abstract class Prompt extends Base {
     /**Returns the name used to define the element */
     static elementName() {
-        return 'container';
+        return '@abstract@';
     }
     /**Returns the namespace override for the element*/
     static elementNameSpace() {
@@ -15,22 +14,4 @@ export class Container extends Base {
     constructor() {
         super();
     }
-
-    /**Returns the zindex of the context menu container default is 999999999 */
-    get zIndex() {
-        return parseInt(this.style.zIndex);
-    }
-
-    /**Changes z index of the context menu container, this should always be the highest element of the dom */
-    set zIndex(z: number) {
-        this.style.zIndex = String(z);
-    }
 }
-defineElement(Container);
-
-events.on('documentAdded', (e) => {
-    (<any>e.data)["@chocolatelibui/prompts"] = e.data.documentElement.appendChild(new Container);
-});
-forDocuments((doc) => {
-    (<any>doc)["@chocolatelibui/prompts"] = doc.documentElement.appendChild(new Container);
-})
